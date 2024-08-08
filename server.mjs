71e51beff696620f4cd82263e1bf56a0597e2a8d
Will3m7 <Ottvillem3000@gmail.com>
2024-08-08 23:45:30 +0300

    .

--- server.mjs
+++ server.mjs
@@ -6,6 +6,6 @@ const cors = require('cors')
 app.use(cors())
 
 app.get("/" async (req, res) => {
-    const response = await fetch("https://proxyserver-bice.vercel.app/")
+    const response = await fetch("https://uptime-mercury-api.azurewebsites.net/webparser")
     res.json(await response.json())
 })
